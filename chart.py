a4ade4c2f3027b1a401d5a7085e05b81d320aae2
Garrett Hurst <pidgions21@gmail.com>
2024-09-13 16:01:46 -0400

    added stock watchlists and breakdowns

--- chart.py
+++ chart.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
+from scipy.optimize import minimize
 
 import re
 import os
 import datetime
 from collections import OrderedDict
+import json
 
 from plotting import (plot_stocks_gui, plot_sankey)
 
@@ -65,6 +67,11 @@ def select_sectors(df:pd.DataFrame) -> dict:
         else:              data[sect]  = val
     return data
 
+def get_watchlist_category(watchlist, symbol):
+    for category, data in watchlist.items():
+        if symbol in data['stocks']:
+            return category
+    return "other"
 
 if __name__ == '__main__':
     # load stocks and sector mappings into data frames
@@ -89,8 +96,6 @@ if __name__ == '__main__':
     df_portfolio['Category']      = df_portfolio['Symbol'].apply(lambda x: get_investimet_type(x, df_sectors))
     df_portfolio['Sector']        = df_portfolio['Symbol'].apply(lambda x: get_sector(x, df_sectors))
     
-    print(df_portfolio)
-
     # create sankey to visulize account breakdowns
     plot_sankey(df_portfolio, excluded_accts=['Cash Management (Individual - TOD)',])
 
@@ -99,6 +104,59 @@ if __name__ == '__main__':
     stock_sectors = select_sectors(df_portfolio)
     plot_stocks_gui(stocks, stock_sectors)
 
+    watchlist_path = 'watchlist.json'
+    if watchlist_path in os.listdir():
+        # load watchlist data from json file
+        with open(watchlist_path, 'r') as f:
+            watchlist = json.load(f)
 
+        # update df with watchlist categories
+        df_stocks = df_portfolio[df_portfolio['Category'] == 'Stock']
+        df_stocks['Watchlist Category'] = df_stocks['Symbol'].apply(lambda x: get_watchlist_category(watchlist, x))
+        
+        # calculate stock percentage of non-watchlist positions
+        watchlist['other'] = {
+            'goal': 1 - sum([v['goal'] for v in watchlist.values()])
+        }
 
+        # update watchlist dict with actual-percentage of categories and 
+        # the current value of each category
+        stocks_value = df_stocks['Current Value'].sum()
+        for cat, data in watchlist.items():
+            data['value']  = df_stocks[df_stocks['Watchlist Category'] == cat]['Current Value'].sum()
+            data['actual'] = data['value'] / stocks_value
+
+        # define function to minimize solving for X
+        def objective(X, watchlist):
+            values      = np.zeros(len(watchlist))       
+            total_value = 0
+            P_goal      = np.zeros(len(watchlist))
+            for i, data in enumerate(watchlist.values()):
+                new_val = data['value'] + X[i]
+                values[i] = new_val
+                total_value += new_val
+                P_goal[i] = data['goal']
+            
+            P_calc = values / total_value
+            
+            return np.linalg.norm(P_goal - P_calc, ord=1)
+
+        # solve for minimium X in objective constrained to all elements in X > 0
+        # update the watchlist dict once calulations are complete
+        result = minimize(objective, [0,0,0], args=(watchlist,),bounds=[(0, None)])
+        for i, data in enumerate(watchlist.values()):
+            data['add'] = result.x[i]
+
+        # print output real pretty
+        for cat, data in watchlist.items():
+            print(
+                '{:<20} ({:6} %) {:8} + {:8} = {:8} ({:6} %)'.format(
+                    cat, 
+                    round(data['actual']*100, 2), 
+                    round(data['value'], 2),
+                    round(data['add'], 2),
+                    round(data['add']+data['value'], 2),
+                    round(data['goal']*100, 2)
+                    )
+            )
     
